d13255d8ea62a9952de882d7bc63ce623d0a200d
vera-zelyukina <vera.zelyukina@gmail.com>
2025-09-12 15:06:08 +0300

    Ex13

--- user_agent.py
+++ user_agent.py
@@ -1,5 +1,23 @@
 import requests
+import pytest
 
-url = "https://gist.github.com/KotovVitaliy/138894aa5b6fa442163561b5db6e2e26"
-response = requests.get(url)
-print(response.text)
+class TestUserAgent:
+    ua_arr = [
+        ('Mozilla/5.0 (Linux; U;  Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',{'platform': 'Mobile', 'browser': 'No', 'device': 'Android'}),
+        ('Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1',{'platform': 'Mobile', 'browser': 'Chrome', 'device': 'iOS'}),
+         ('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)', {'platform': 'Googlebot', 'browser': 'Unknown', 'device': 'Unknown'}),
+          ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0',{'platform': 'Web', 'browser': 'Chrome', 'device': 'No'}),
+           ('Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1', {'platform': 'Mobile', 'browser': 'No', 'device': 'iPhone'})
+    ]
+
+    @pytest.mark.parametrize("ua_string, expected_value", ua_arr)
+    def test_user_agent(self, ua_string, expected_value):
+        url = "https://playground.learnqa.ru/ajax/api/user_agent_check"
+
+        response = requests.get(url, headers={"User-Agent": ua_string})
+        response_dict = response.json()
+
+        keys = ["device", "browser", "platform"]
+        for key in keys:
+            assert key in response_dict, f"User Agent '{ua_string}' has not '{key}' in the response"
+            assert response_dict[key] == expected_value[key], f"User Agent '{ua_string}' have incorrect '{key}'"
